bf12d1ed323bb1666ecd4ff02d04c3af5ca9a635
Marcel Blijleven <marcelblijleven@gmail.com>
2023-05-10 11:55:31 +0200

    feat: add 'show all' option for favourites

--- src/app/coffee/upload/CountableStats.tsx
+++ src/app/coffee/upload/CountableStats.tsx
@@ -1,4 +1,8 @@
+"use client"
+
 import ProgressBar from "@/components/ProgressBar";
+import {useState} from "react";
+import ClickableText from "@/components/ClickableText";
 
 interface Props {
     label: string;
@@ -6,28 +10,42 @@ interface Props {
 }
 
 export default function CountableStats(props: Props) {
-    let total = 0;
-    const entries = Object.entries<number>(props.countable).map(([key, value]) => {
-        total += value;
+    const [slice, setSlice] = useState<boolean>(true);
+    const slicedLength = 10;
+    const totalEntries = Object.keys(props.countable).length;
+
+    let entries = Object.entries<number>(props.countable).map(([key, value]) => {
         return [key, value]
-    }).slice(0, 10);
+    });
+
+    if (slice) {
+        entries = entries.slice(0, slicedLength);
+    }
+
+    const total = entries.reduce((prev, [_, value]) => prev + value, 0);
 
     const items = entries.map(([key, value]) => {
         return (
             <div key={key} className={"flex gap-2 items-center mb-1"}>
-                <div className={"flex w-3/12 gap-2 justify-between text-sm font-semibold text-gray-900 dark:text-gray-100"}>
+                <div
+                    className={"flex w-3/12 gap-2 justify-between text-sm font-semibold text-gray-900 dark:text-gray-100"}>
                     <p className={"truncate"}>{key}</p>
                     <p className={""}>{value}</p>
                 </div>
-                <ProgressBar total={total} progress={(value as number)} />
+                <ProgressBar total={total} progress={(value as number)}/>
             </div>
         )
-    })
-
+    });
+    console.log(entries.length, slicedLength, slice)
     return (
         <div className={"mb-4"}>
             <h2 className={"text-2xl font-semibold mb-2"}>{props.label}</h2>
             {items}
+            {totalEntries > slicedLength && (
+                <div className={"flex justify-end"}>
+                    <ClickableText text={slice ? "Show all" : "Collapse"} onClick={() => setSlice(!slice)}/>
+                </div>
+            )}
         </div>
     )
 }
