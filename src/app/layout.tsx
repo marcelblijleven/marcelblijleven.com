df6ce27548c2cad3ea7c237046435b2a15d20b2a
Marcel Blijleven <marcelblijleven@gmail.com>
2023-05-12 08:58:06 +0200

    imp: improve styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import type { Metadata } from "next";
 
 const inter = Inter({ subsets: ['latin'] })
+const color = "#0284c7";  // sky 600
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'marcelblijleven.com',
+  themeColor: [
+    {color, media: "(prefers-color-scheme: light)"},
+    {color, media: "(prefers-color-scheme: dark)"},
+  ]
 }
 
 export default function RootLayout({
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,61 +1,25 @@
 "use client"
 
 import Link from "next/link";
-import Button from "@/components/Button";
-import HamburgerIcon from "@/components/icons/HamburgerIcon";
-import {useState} from "react";
-import CloseIcon from "@/components/icons/CloseIcon";
 
 interface Props {
 
 }
 
 export default function Header(props: Props) {
-    const [menuOpen, setMenuOpen] = useState<boolean>(false);
-    const onMenuClick = () => setMenuOpen(!menuOpen);
-    const navLinks = (
-        <div className={"flex gap-4"}>
-            <Link href={"/"}>Home</Link>
-            <Link href={"/coffee"}>Coffee</Link>
-            <Link href={"/about"}>About</Link>
-        </div>
-    )
-
+    // Note: z-10 on header because the progress bars from /coffee would overlap on Safari
     return (
         <>
-            <header className={"sticky top-0 w-full bg-slate-50 dark:bg-slate-900 border-b border-gray-300/25 mb-2"}>
-                <div className={"flex justify-between items-center max-w-7xl mx-auto px-4 sm:px-6 md:px-8 h-14"}>
-                    <nav className={"hidden md:flex"}>
-                        <div
-                            className={"flex gap-6 text-sm leading-6 font-semibold text-gray-900 dark:text-gray-100"}>
-                            {navLinks}
+            <header className={"sticky top-0 z-10 w-full bg-white dark:bg-slate-900 mb-2"}>
+                <div className={"flex justify-between items-center max-w-7xl mx-auto px-8 h-14"}>
+                    <Link className={"hover:underline hover:decoration-sky-600 hover:decoration-4 font-semibold text-lg"} href={"/"}>Marcel Blijleven</Link>
+                    <nav>
+                        <div className={"flex gap-4 md:gap-6 font-semibold"}>
+                            <Link className={"hover:underline hover:decoration-4 hover:decoration-sky-600"} href={"/coffee"}>Coffee</Link>
                         </div>
                     </nav>
-                    <div className={"flex sm:justify-self-start md:justify-self-end font-semibold"}>
-                        Marcel Blijleven
-                    </div>
-
-                    <div className={"flex justify-self-end md:hidden"}>
-                        <Button variant={"icon"} onClick={onMenuClick}
-                                icon={menuOpen ? <CloseIcon/> : <HamburgerIcon/>}/>
-                    </div>
                 </div>
             </header>
-            {menuOpen && (
-                <div className={"absolute top-14 left-0 h-screen w-full h-full z-10 bg-white dark:bg-slate-900"}>
-                    <div className={"flex flex-col w-full divide-y-2 font-semibold"}>
-                        <div className={"flex items-center justify-center flex-grow h-14"}>
-                            <Link href={"/"}>Home</Link>
-                        </div>
-                        <div className={"flex items-center justify-center flex-grow h-14"}>
-                            <Link href={"/coffee"}>Coffee</Link>
-                        </div>
-                        <div className={"flex items-center justify-center flex-grow h-14"}>
-                            <Link href={"/about"}>About</Link>
-                        </div>
-                    </div>
-                </div>
-            )}
         </>
     )
 }
