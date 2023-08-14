7b067e9a4a81509ff6aaf4d6fb8411d9d0858fe4
Marcel Blijleven <marcelblijleven@gmail.com>
2023-08-14 17:18:50 +0200

    chore: format file

--- src/components/terminal/index.tsx
+++ src/components/terminal/index.tsx
@@ -119,9 +119,9 @@ const techText = [
 
 function MiniTerminal({
                           onClick,
-    minimised = false,
+                          minimised = false,
                           children,
-                            className,
+                          className,
                       }: { onClick?: () => void, minimised?: boolean, children: ReactNode, className?: string }) {
     return (
         <motion.div
@@ -137,7 +137,8 @@ function MiniTerminal({
                 <div className={"h-1.5 w-1.5 rounded-full bg-[#00CA4E]"}/>
             </div>
             {children}
-            {minimised && <div className={"absolute -bottom-[12px] left-0 right-0 mx-auto h-1 w-1 rounded-full bg-cyan-500 shadow-lg shadow-cyan-500/50"} />}
+            {minimised && <div
+              className={"absolute -bottom-[12px] left-0 right-0 mx-auto h-1 w-1 rounded-full bg-cyan-500 shadow-lg shadow-cyan-500/50"}/>}
         </motion.div>
     )
 }
@@ -160,7 +161,7 @@ function TerminalWindow() {
                 className={"absolute flex items-center justify-center space-x-4 left-0 right-0 mx-auto bottom-0 w-full"}>
                 <a target="_blank" href="https://github.com/marcelblijleven" rel="noopener noreferrer">
                     <MiniTerminal className={"bg-gray-500 dark:bg-gray-500 text-background dark:text-foreground"}>
-                        <Github className={"w-6 h-6 mt-2"} />
+                        <Github className={"w-6 h-6 mt-2"}/>
                     </MiniTerminal>
                 </a>
                 <MiniTerminal onClick={() => setMinimised(false)} minimised={minimised}>
@@ -171,9 +172,13 @@ function TerminalWindow() {
                     </svg>
                 </MiniTerminal>
                 <a target="_blank" href="https://www.linkedin.com/in/marcelblijleven/" rel="noopener noreferrer">
-                <MiniTerminal className={"bg-[#0a66c2] dark:bg-[#0a66c2]"}>
-                    <svg className={"w-6 h-6 mt-2 fill-background dark:fill-foreground"} role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn</title><path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/></svg>
-                </MiniTerminal>
+                    <MiniTerminal className={"bg-[#0a66c2] dark:bg-[#0a66c2]"}>
+                        <svg className={"w-6 h-6 mt-2 fill-background dark:fill-foreground"} role="img"
+                             viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>LinkedIn</title>
+                            <path
+                                d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+                        </svg>
+                    </MiniTerminal>
                 </a>
             </div>
             <motion.div
@@ -181,7 +186,7 @@ function TerminalWindow() {
                 variants={variants}
                 animate={minimised ? 'minimised' : 'maximised'}
                 transition={{duration: 0.2}}
-                className={cn("absolute bottom-0 z-10 left-0 right-0 mx-auto min-h-[450px] md:min-h-[700px] bg-orange-100 dark:bg-gray-900 rounded-lg overflow-hidden", !fullSize ? "w-[95%]" : "w-[100%]")}
+                className={cn("absolute bottom-0 z-10 left-0 right-0 mx-auto min-h-[450px] md:min-h-[600px] bg-orange-100 dark:bg-gray-900 rounded-lg overflow-hidden", !fullSize ? "w-[95%]" : "w-[100%]")}
                 onMouseLeave={() => setHoverIndex(null)}
             >
                 <Tabs.Root value={tab} onValueChange={(val) => setTab(val)} className={"overflow-unset"}>
@@ -190,7 +195,7 @@ function TerminalWindow() {
                                 toggleMinimised={() => setMinimised(!minimised)}/>
                         <TabsList tabs={["aboutme.md", "tech.md"]}/>
                         <Tabs.Content value={"aboutme.md"}>
-                            <div className={"h-[450px] md:h-[700px] overflow-y-scroll"}>
+                            <div className={"h-[450px] md:h-[600px] overflow-y-scroll"}>
                                 {text.map((value, index) => (
                                     <Row key={`${value}_${index}`}
                                          text={value}
@@ -201,7 +206,7 @@ function TerminalWindow() {
                             </div>
                         </Tabs.Content>
                         <Tabs.Content value={"tech.md"}>
-                            <div className={"h-[450px] md:h-[700px] overflow-y-scroll"}>
+                            <div className={"h-[450px] md:h-[600px] overflow-y-scroll"}>
                                 {techText.map((value, index) => (
                                     <Row key={`${value}_${index}`}
                                          text={value}
@@ -221,7 +226,7 @@ function TerminalWindow() {
 
 function Terminal() {
     return (
-        <div className={"relative w-full max-w-[1000px] h-[600px] md:h-[700px]"}>
+        <div className={"relative w-full max-w-[1000px] h-[600px] md:h-[600px]"}>
             <TerminalWindow/>
         </div>
     )
