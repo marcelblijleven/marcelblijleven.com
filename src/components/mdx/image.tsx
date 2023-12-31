ccfb6672300e19e8666c415580154bf6889d15f4
Marcel Blijleven <marcelblijleven@gmail.com>
2023-12-31 16:23:35 +0100

    style: update image styling

--- src/components/mdx/image.tsx
+++ src/components/mdx/image.tsx
@@ -1,5 +1,5 @@
 import NextImage, { ImageProps } from "next/image";
 
-const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} />;
+const Image = ({ ...rest }: ImageProps) => <NextImage {...rest} className={"my-4 rounded-lg"} />;
 
 export default Image;
