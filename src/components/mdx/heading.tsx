a141c127f3572fbf2f99a31caaded0109b4658d0
Marcel Blijleven <marcelblijleven@gmail.com>
2024-02-01 23:07:33 +0100

    style: increase space below headings

--- src/components/mdx/heading.tsx
+++ src/components/mdx/heading.tsx
@@ -1,11 +1,11 @@
 export function Heading1({ children }) {
-  return <h1 className={"text-3xl font-semibold mt-2"}>{children}</h1>;
+  return <h1 className={"text-3xl font-semibold mt-4"}>{children}</h1>;
 }
 
 export function Heading2({ children }) {
-  return <h2 className={"text-2xl font-semibold mt-2"}>{children}</h2>;
+  return <h2 className={"text-2xl font-semibold mt-4"}>{children}</h2>;
 }
 
 export function Heading3({ children }) {
-  return <h3 className={"text-xl font-semibold mt-2"}>{children}</h3>;
+  return <h3 className={"text-xl font-semibold mt-4"}>{children}</h3>;
 }
