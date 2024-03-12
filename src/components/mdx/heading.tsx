a920fcb3cfe60964ff8c347d58fbfac1be849124
Marcel Blijleven <marcelblijleven@gmail.com>
2024-03-12 08:33:13 +0100

    style: increase margin above headings

--- src/components/mdx/heading.tsx
+++ src/components/mdx/heading.tsx
@@ -1,15 +1,15 @@
 export function Heading1({ children }) {
-  return <h1 className={"text-3xl font-semibold mt-4"}>{children}</h1>;
+  return <h1 className={"text-3xl font-semibold mt-6"}>{children}</h1>;
 }
 
 export function Heading2({ children }) {
-  return <h2 className={"text-2xl font-semibold mt-4"}>{children}</h2>;
+  return <h2 className={"text-2xl font-semibold mt-6"}>{children}</h2>;
 }
 
 export function Heading3({ children }) {
-  return <h3 className={"text-xl font-semibold mt-4"}>{children}</h3>;
+  return <h3 className={"text-xl font-semibold mt-6"}>{children}</h3>;
 }
 
 export function Heading4({ children }) {
-  return <h4 className={"text-lg font-semibold mt-4"}>{children}</h4>;
+  return <h4 className={"text-lg font-semibold mt-6"}>{children}</h4>;
 }
