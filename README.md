5637ca6b4d1d8d24d32c9d13cecf19f136a4908d
Marcel Blijleven <marcelblijleven@gmail.com>
2024-04-02 22:26:23 +0200

    chore: remove remaining nextjs files

--- README.md
+++
@@ -1,1 +0,0 @@
-# marcelblijleven.com
--- data/header-nav-links.ts
+++
@@ -1,9 +0,0 @@
-const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/coffee', title: 'Coffee' },
-  // { href: '/activities', title: 'Activities' },
-  // { href: '/tags', title: 'Tags' },
-]
-
-export default headerNavLinks
